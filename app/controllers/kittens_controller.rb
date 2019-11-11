60a757784a3ba952bbd59ac94b9a17faca2049d5
Robin Unger <robin.e.unger@gmail.com>
2019-11-11 16:08:57 -0600

    New kitten form creates a kitten

--- app/controllers/kittens_controller.rb
+++ app/controllers/kittens_controller.rb
@@ -11,6 +11,12 @@ class KittensController < ApplicationController
   end
 
   def create
+    @kitten = Kitten.new(kitten_params)
+    if @kitten.save
+      redirect_to @kitten
+    else
+      render :new
+    end
   end
 
   def edit
@@ -21,4 +27,9 @@ class KittensController < ApplicationController
 
   def delete
   end
+
+  private
+    def kitten_params
+      params.require(:kitten).permit(:name, :age, :cuteness, :softness)
+    end
 end
