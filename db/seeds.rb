073b4f4e91b7be47bffc3578339e83652537f65c
Robin Unger <robin.e.unger@gmail.com>
2019-11-11 15:42:31 -0600

    List kittens on index page

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Kitten.create!(name: "Mash", age: 3, cuteness: 10, softness: 5)
+Kitten.create!(name: "Fran", age: 1, cuteness: 10, softness: 10)
+Kitten.create!(name: "Bear", age: 2, cuteness: 5, softness: 3)
+Kitten.create!(name: "Melt", age: 3, cuteness: 10, softness: 1)
+Kitten.create!(name: "Herc", age: 4, cuteness: 1, softness: 1)
